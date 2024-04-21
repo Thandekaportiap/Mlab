afe26bb2492205a2272a594e67cb1184c0873751
thandekapp <thandekaportiap07@gmail>
2024-04-21 16:35:17 +0200

    first js file

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const name = "Thandeka Portia P";
+const lastName = "Mazibuko";
+const age = 26;
+const email = thandekaportiap07@gmail.com;
+
+
+const project = [weatherApp, surveyForm];
+console.log(project);
