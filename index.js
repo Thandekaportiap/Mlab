d94162c79e02bbc4b01b19a1e43112ed0127a72d
thandekapp <thandekaportiap07@gmail>
2024-04-22 17:53:39 +0200

    obj

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
-const name = "Thandeka Portia P";
-const lastName = "Mazibuko";
-const age = 26;
-const email = thandekaportiap07@gmail.com;
+const personalInfo = {
+            firstName: "Thandeka Porti",
+            lastName: "Mazibuko",
+            age: 26,
+            bio: "I am a passionate web developer with expertise in front-end technologies.",
+            education: "Self taught",
+            email: "thandekaportiap@gmail.com",
+            phone: 0826325121,
+            currentTown: "Estcourt"
+            };
+ console.log(personalInfo);           
 
 
 const project = [weatherApp, surveyForm];
